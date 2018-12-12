5395a0e45f98c3fab49c082d8dfa037e0b4307fd
listoky <45196015+listoky@users.noreply.github.com>
2018-12-12 02:18:27 +0100

    Add files via upload

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,6 +1,46 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  $(document).ready(function()  {
+    var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      centeredSlides: true,
+      speed: 1000,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      on: {
+        init: function() {
+          $('.first-slide').each(function() {
+              $(this).find('.innerslider').fadeIn(1000);
+          })
+        },
+        slideChangeTransitionEnd: function() {
+          $('.swiper-slide').each(function() {
+            if ($(this).index() === swiper.activeIndex) {
+                // Fadein in active slide
+                $(this).find('.innerslider').fadeIn(1000);
+            } else {
+                // Fadeout in inactive slides
+                $(this).find('.innerslider').fadeOut(1000);
+            }
+          });
+        }
+      },
+    });
+  })
+
+  $.stellar({
+      horizontalScrolling: false,
+      verticalOffset: 40
+    });
+
+ 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
